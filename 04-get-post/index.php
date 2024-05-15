34d6f2797dd934c8987660ce49bd99185292e6eb
miyimann <miyiman42@gmail.com>
2024-05-15 16:08:28 -0500

    feat: get-post

--- 04-get-post/index.php
+++ 04-get-post/index.php
@@ -1,6 +1,10 @@
 <?php
+$userName = $_POST['username'];
+$userEmail = $_POST['useremail']
 
-
+echo $userName;
+echo '<br>';
+echo $userEmail;
 
 
 ?>
@@ -14,8 +18,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <style>
+        body{background-color: <?php echo $_GET['color'] ?> ; }
+    </style>
 </head>
 <body>
+    <form action="index.php"></form>
 
 
 
